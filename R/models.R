d5a9cc58aca6bad9f60a9ab3d88bb0add34fdd1c
sebsilas <sebsilas@gmail.com>
2024-08-14 13:57:23 +0200

    Push data

--- R/models.R
+++ R/models.R
@@ -8,3 +8,13 @@
 
 #' The training dataset
 "vowel_metadata_with_audio_features_range_restricted_without_AEIOU"
+
+#' Baseline Shapley values
+"shap_values_baseline"
+
+#' Audio features summarised
+"audio_features_summarised"
+
+#' Audio features summarised by syllable
+"audio_features_summarised_by_syllable"
+
